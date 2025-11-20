f6aa61fd2ecdd5610f8f58bdbf9a4d2be96685ac
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-11-20 21:03:48 +0800

    update

--- neo4j/import_data_fundamentals/Importing_Data_Fundamentals.mm
+++ neo4j/import_data_fundamentals/Importing_Data_Fundamentals.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Importing Data Fundamentals&#xa;(Neo4j GraphAcademy)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1762885523335" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="0.75131476">
+<node TEXT="Importing Data Fundamentals&#xa;(Neo4j GraphAcademy)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1762885523335" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff">
     <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sky_element_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
     <tags category_separator="::"/>
 
@@ -97,7 +97,7 @@
 </node>
 </node>
 <node TEXT="2 Neo4j Data Importer" POSITION="bottom_or_right" ID="ID_1023119852" CREATED="1762885619876" MODIFIED="1762885655889">
-<node TEXT="2.1 Data Importer" ID="ID_709867865" CREATED="1762885720811" MODIFIED="1762885724894">
+<node TEXT="2.1 Data Importer" FOLDED="true" ID="ID_709867865" CREATED="1762885720811" MODIFIED="1762885724894">
 <node TEXT="(1) Access Data Importer" ID="ID_1080945931" CREATED="1763396745095" MODIFIED="1763396772111" LINK="https://workspace.neo4j.io/workspace/import"/>
 <node TEXT="(2) Creating Nodes" FOLDED="true" ID="ID_1688475345" CREATED="1763396772907" MODIFIED="1763396777617">
 <node TEXT="(2)-1 Connect to your sandbox" ID="ID_144458664" CREATED="1763396783997" MODIFIED="1763396794393"/>
@@ -110,8 +110,14 @@
 <node TEXT="(4) Backup your work through &quot;Download model (with data)&quot;" ID="ID_614372349" CREATED="1763396917515" MODIFIED="1763396938471"/>
 <node TEXT="(5) Import locally through &quot;LOAD CSV&quot;" ID="ID_704603035" CREATED="1763396952173" MODIFIED="1763396972431"/>
 </node>
-<node TEXT="2.2 Properties and Types" ID="ID_1133221785" CREATED="1762885725069" MODIFIED="1762885729545"/>
-<node TEXT="2.3 Add Movie Nodes" ID="ID_1399563396" CREATED="1762885729718" MODIFIED="1762885741632"/>
+<node TEXT="2.2 Properties and Types" FOLDED="true" ID="ID_1133221785" CREATED="1762885725069" MODIFIED="1762885729545">
+<node TEXT="Using Data Importer" ID="ID_1582852320" CREATED="1763512683228" MODIFIED="1763512695021"/>
+<node TEXT="Using &quot;LOAD CSV&quot;" ID="ID_445092936" CREATED="1763512695213" MODIFIED="1763512700096"/>
+</node>
+<node TEXT="2.3 Add Movie Nodes" ID="ID_1399563396" CREATED="1762885729718" MODIFIED="1762885741632">
+<node TEXT="Using Data Importer" POSITION="bottom_or_right" ID="ID_1739200279" CREATED="1763512683228" MODIFIED="1763512695021"/>
+<node TEXT="Using &quot;LOAD CSV&quot;" POSITION="bottom_or_right" ID="ID_459975127" CREATED="1763512695213" MODIFIED="1763512700096"/>
+</node>
 <node TEXT="2.4 Unique IDs and Constraints" ID="ID_1683224451" CREATED="1762885741735" MODIFIED="1762885750033"/>
 <node TEXT="2.5 Creating Relationships" ID="ID_76099303" CREATED="1762885750209" MODIFIED="1762885754862"/>
 <node TEXT="2.6 Add Directed Relationship" ID="ID_274362000" CREATED="1762885755039" MODIFIED="1762885761881"/>
