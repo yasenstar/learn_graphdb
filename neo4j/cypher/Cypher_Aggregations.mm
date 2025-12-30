25614ea7dd2caf90e0646251052645ce1034a1a4
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-12-30 15:27:09 +0800

    Create Cypher_Aggregations.mm

---
+++ neo4j/cypher/Cypher_Aggregations.mm
@@ -0,0 +1,187 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Cypher_Aggregations&#xa;(Neo4j Graph Academy)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1767063050546">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#eeeeeeff" zoom="0.9090909">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/light_grey_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_364653791" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#676767" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_364653791" STARTINCLINATION="122.25 pt;0 pt;" ENDINCLINATION="119.25 pt;35.25 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1533685614" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_1533685614"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#cc0033"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#222222" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="1. Understanding Aggregation in Cypher" POSITION="bottom_or_right" ID="ID_1353126640" CREATED="1767063104049" MODIFIED="1767063540484"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Aggregation in Cypher means that during a query, the data retrieved is collected or counted. The aggregation of values or counts are either returned from the query or are used as input for the next part of a multi-step query. If you view the execution plan for the query, you will see that rows are returned for a step in the query. The rows that are returned in a query step are an aggregation of property values, nodes, relationships, or paths in the graph. There are some best practices for aggregating data during your query that you can better understand with examples and explanations of how the query works at runtime.
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="1.1 Creating Lists" ID="ID_123729866" CREATED="1767063131217" MODIFIED="1767063135779">
+<node TEXT="A list is an array that contains elements. The elements in a list need not all be of the same type." ID="ID_150817629" CREATED="1767065018609" MODIFIED="1767065048421"/>
+<node TEXT="Most queries use collect() to create lists, but there&apos;re other functions you can use as well, such as nodes()." ID="ID_875158660" CREATED="1767065248817" MODIFIED="1767065283027">
+<node TEXT="collect() returns a list of elements, which is used to create a list at any time during the query." ID="ID_910298048" CREATED="1767065575388" MODIFIED="1767065605263"/>
+<node TEXT="When you create a list during the query, aggregation occurs." ID="ID_305010646" CREATED="1767065605548" MODIFIED="1767065624336"/>
+</node>
+<node TEXT="During aggregation, the graph engine typically groups data based upon some value in a row." ID="ID_1877126064" CREATED="1767065679275" MODIFIED="1767065710127">
+<node ID="ID_1874473668" CREATED="1767065781416" MODIFIED="1767065795808"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The non-aggregated value is the <span style="font-weight: bold;">grouping key</span><span style="font-weight: normal;">.</span>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Collecting Nodes" ID="ID_1899320626" CREATED="1767075197485" MODIFIED="1767075204895">
+<node TEXT="MATCH (m:Movie)" ID="ID_1840564092" CREATED="1767075204901" MODIFIED="1767075216091">
+<node TEXT="This Match retrieve all Movie nodes" ID="ID_1415717096" CREATED="1767075216096" MODIFIED="1767075229105"/>
+</node>
+<node TEXT="UNWIND m.languages AS language" ID="ID_880097630" CREATED="1767075230301" MODIFIED="1767075242573">
+<node TEXT="Each value in the languages property is returned" ID="ID_1979936409" CREATED="1767075242576" MODIFIED="1767075253584"/>
+</node>
+<node TEXT="WITH language, collect(m) AS movies" ID="ID_1232931079" CREATED="1767075260460" MODIFIED="1767075272669">
+<node TEXT="Collect the movie nodes and use the value of language as the grouping key. that is all of the movies with a value of English will be collected into a list." ID="ID_361131703" CREATED="1767075272673" MODIFIED="1767075313615"/>
+</node>
+<node TEXT="MERGE (l:Language {name:language})" ID="ID_1618212457" CREATED="1767075314893" MODIFIED="1767075330655">
+<node TEXT="Create the Language node using the language value" ID="ID_776123247" CREATED="1767075330660" MODIFIED="1767075339840"/>
+</node>
+<node TEXT="WITH l, movies&#xa;UNWIND movies AS m" ID="ID_1016546801" CREATED="1767075341437" MODIFIED="1767075367323">
+<node TEXT="With the language value and the list of Movie nodes for that language, we unwind the movie nodes into rows." ID="ID_1118977299" CREATED="1767075367331" MODIFIED="1767075387855"/>
+</node>
+<node TEXT="WITH l, m&#xa;MERGE (l)&lt;-[:SPEAKS]-(m)" ID="ID_1062467475" CREATED="1767075397197" MODIFIED="1767075417005">
+<node TEXT="With each language and row for each Movie, we create the relationship between the Language node and the Movie node" ID="ID_1628749666" CREATED="1767075417013" MODIFIED="1767075446829"/>
+</node>
+</node>
+<node TEXT="Collecting Relationships" ID="ID_1233410535" CREATED="1767075479308" MODIFIED="1767075484575">
+<node TEXT="Use endNode() to return the node as the end of the relationship" ID="ID_1939690916" CREATED="1767075616907" MODIFIED="1767075637983"/>
+</node>
+<node TEXT="Collecting paths in a query" ID="ID_1011654187" CREATED="1767075678346" MODIFIED="1767075684734">
+<node TEXT="nodes() function returns the list of all nodes in a path" ID="ID_721914261" CREATED="1767075696221" MODIFIED="1767075709389"/>
+</node>
+</node>
+<node TEXT="1.2 Creating and Working with Lists" ID="ID_1816939292" CREATED="1767063135936" MODIFIED="1767063143637">
+<node TEXT="Movies with 1 or 2 Actors&#xa;Return the movies that have one or two actors, return movie title, the year the movie was released and the actors for the movie&#xa;Answer: 139" ID="ID_15081800" CREATED="1767077810685" MODIFIED="1767077875105">
+<node TEXT="MATCH (a:Actor)-[:ACTED_IN]-&gt;(m:Movie)&#xa;WITH m, collect(a) AS Actors&#xa;WHERE size(Actors) = 1 OR size(Actors) = 2&#xa;RETURN m.title AS Title, m.released AS ReleasedYear, Actors" ID="ID_564303594" CREATED="1767077811565" MODIFIED="1767077814081"/>
+</node>
+</node>
+<node TEXT="1.3 Using Aggregation to Count" ID="ID_411478847" CREATED="1767063143808" MODIFIED="1767063151188">
+<node TEXT="count() function performs a count of properties, nodes, relationships, paths, or rows during query processing." ID="ID_998802559" CREATED="1767077897083" MODIFIED="1767077931551">
+<node TEXT="count(*) returns the number of rows retrieved in the query" ID="ID_749278782" CREATED="1767078114502" MODIFIED="1767078129674"/>
+</node>
+<node TEXT="Count the movie titles that have been rated" ID="ID_39432412" CREATED="1767079565915" MODIFIED="1767079581050">
+<node TEXT="MATCH (u:User)-[:RATED]-&gt;(m:Movie)&#xa;RETURN count(DISTINCT m.title)" ID="ID_1944686335" CREATED="1767079581055" MODIFIED="1767079608910"/>
+</node>
+</node>
+<node TEXT="1.4 Counting Nodes - count()" ID="ID_306110535" CREATED="1767063164223" MODIFIED="1767063498126"/>
+<node TEXT="1.5 Using Pattern Comprehension" ID="ID_588896211" CREATED="1767063169648" MODIFIED="1767063179650"/>
+<node TEXT="1.6 Younger Actors" ID="ID_239745448" CREATED="1767063179791" MODIFIED="1767063187441"/>
+</node>
+<node TEXT="2. Working with Lists" POSITION="bottom_or_right" ID="ID_884646455" CREATED="1767063119647" MODIFIED="1767063123203">
+<node TEXT="2.01 Scalar List Functions" ID="ID_7515883" CREATED="1767063193231" MODIFIED="1767063205617"/>
+<node TEXT="2.02 Oldest Actor" ID="ID_1980421756" CREATED="1767063205761" MODIFIED="1767063211775"/>
+<node TEXT="2.03 Total imdbVotes for Tom Hanks" ID="ID_1796447946" CREATED="1767063211934" MODIFIED="1767063230689"/>
+<node TEXT="2.04 Functions that Return Lists" ID="ID_714867403" CREATED="1767063230848" MODIFIED="1767063237217"/>
+<node TEXT="2.05 Latest Movie Review" ID="ID_822847681" CREATED="1767063237376" MODIFIED="1767063243554"/>
+<node TEXT="2.06 Reviewed Same Movies" ID="ID_1840672637" CREATED="1767063243744" MODIFIED="1767063251346"/>
+<node TEXT="2.07 Functions to Transform Element Types" ID="ID_997021319" CREATED="1767063251503" MODIFIED="1767063261983"/>
+<node TEXT="2.08 Update Movie Reviews" ID="ID_1871230436" CREATED="1767063262143" MODIFIED="1767063268466"/>
+<node TEXT="2.09 Testing Lists with Predicate Functions" ID="ID_1604442509" CREATED="1767063268623" MODIFIED="1767063276142"/>
+<node TEXT="2.10 Tom Hanks Movies in 1995" ID="ID_1365682223" CREATED="1767063276271" MODIFIED="1767063285665"/>
+<node TEXT="2.11 Using List Comprehension to Aggregate" ID="ID_1906330713" CREATED="1767063285805" MODIFIED="1767063295037"/>
+<node TEXT="2.12 Ages of Actors in Movies" ID="ID_1237566006" CREATED="1767063295165" MODIFIED="1767063305393"/>
+</node>
+<node TEXT="3. Aggregating Functions" POSITION="bottom_or_right" ID="ID_932771011" CREATED="1767063123361" MODIFIED="1767063129811">
+<node TEXT="3.1 Adding Values - sum()" ID="ID_242931258" CREATED="1767063311644" MODIFIED="1767063462851"/>
+<node TEXT="3.2 Years Since Movie Reviewed" ID="ID_200375611" CREATED="1767063318173" MODIFIED="1767063327952"/>
+<node TEXT="3.3 Averaging Values - avg() / stddev()" ID="ID_65897062" CREATED="1767063328125" MODIFIED="1767063477086"/>
+<node TEXT="3.4 Average Reviews per User" ID="ID_1447238987" CREATED="1767063334125" MODIFIED="1767063341425"/>
+<node TEXT="3.5 Calculating Min/Max Values - min() / max()" ID="ID_118598952" CREATED="1767063341582" MODIFIED="1767063486939"/>
+<node TEXT="3.6 Movies with Lowest Ratings" ID="ID_1960797264" CREATED="1767063349325" MODIFIED="1767063358781"/>
+<node TEXT="3.7 Working with Percentages and Percentiles" ID="ID_293727065" CREATED="1767063358907" MODIFIED="1767063370864"/>
+<node TEXT="3.8 75% of Actors Born After a Year" ID="ID_320381927" CREATED="1767063371037" MODIFIED="1767063381550"/>
+</node>
+</node>
+</map>
